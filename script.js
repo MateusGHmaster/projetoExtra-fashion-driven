b33df5623186143361a2630667048ef2c5dd33f6
Mateus <mateusoliveira469@gmail.com>
2022-02-24 14:46:05 -0400

    simple-html

--- script.js
+++ script.js
@@ -6,9 +6,22 @@ function askUserName () {
     }
 }
 
+function getLastShirts () {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/shirts-api/shirts");
+    promise.then((response) => {
+        let lastShirt = document.querySelector('.shirts');
+        lastShirt.innerHTML = `
+        
 
 
+        `;
+    });
+}
 
+function postLastShirt () {
+    const promise = axios.post("https://mock-api.driven.com.br/api/v4/shirts-api/shirts");
+    promise.then();
+}
 
 
 
