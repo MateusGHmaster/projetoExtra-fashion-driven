797fb31cfcb07de03adf904e44593313e5a94e85
Mateus <mateusoliveira469@gmail.com>
2022-02-24 14:00:32 -0400

    ask-user-name

---
+++ script.js
@@ -0,0 +1,15 @@
+let userName = '';
+
+function askUserName () {
+    while (userName === '') {
+        userName = prompt("Por favor, informe-nos o seu nome de usuário.             (／・ω・)／");
+    }
+}
+
+
+
+
+
+
+
+askUserName();
